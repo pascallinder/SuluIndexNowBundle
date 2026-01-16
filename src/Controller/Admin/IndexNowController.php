0f49b5ad914779d8a5d6729202252e6b01f6e8cb
pascallinder <pascal.linder97@hotmail.com>
2026-01-16 14:36:37 +0100

    Update IndexNowController.php

--- src/Controller/Admin/IndexNowController.php
+++ src/Controller/Admin/IndexNowController.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class IndexNowController extends AbstractController{
     public function __construct(
