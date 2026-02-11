7f5e095a33a47f9dc72f5d508af51660360da1e6
pascallinder <pascal.linder97@hotmail.com>
2026-02-11 23:56:51 +0100

    revert some changes

--- Resources/js/src/components/view/IndexNow/IndexNowConfig.js
+++ Resources/js/src/components/view/IndexNow/IndexNowConfig.js
@@ -11,7 +11,6 @@ import './indexNowConfig.scss';
 class IndexNowConfig extends React.Component {
     @observable loading = false;
     @observable data = {urls: [], responses: {}};
-    @observable error = null;
 
     componentDidMount() {
         this.loadUrls().then();
@@ -19,7 +18,6 @@ class IndexNowConfig extends React.Component {
 
     @action loadUrls = () => {
         this.loading = true;
-        this.error = null;
 
         return Requester.get("/admin/api/index-now/urls")
             .then(action((response) => {
@@ -27,7 +25,6 @@ class IndexNowConfig extends React.Component {
                 })
             )
             .catch((e) => {
-                this.error = translate("app.index_now_error_loading");
                 console.error("Error while loading usage data from server.", e);
             })
             .finally(
@@ -38,7 +35,6 @@ class IndexNowConfig extends React.Component {
     }
     @action indexNow = () => {
         this.loading = true;
-        this.error = null;
 
         return Requester.post("/admin/api/index-now/start")
             .then(
@@ -47,7 +43,6 @@ class IndexNowConfig extends React.Component {
                 })
             )
             .catch((e) => {
-                this.error = translate("app.index_now_error_submit");
                 console.error("Error while loading usage data from server.", e);
             })
             .finally(
@@ -58,19 +53,15 @@ class IndexNowConfig extends React.Component {
     };
 
     render() {
-        const {urls} = this.data;
-        const responses = this.data.responses || {};
-        const batchKeys = Object.keys(responses);
-        const engines = batchKeys.flatMap((batchKey) => Object.keys(responses[batchKey] || {}));
+        const {urls, responses} = this.data;
+        const responsesMap = Array.isArray(responses)
+            ? responses.reduce((acc, batch) => Object.assign(acc, batch || {}), {})
+            : (responses || {});
+        const engines = Object.keys(responsesMap);
         return (
             <div>
                 <h1>{translate("app.index_now_config_headline")}</h1>
                 <p>{translate("app.index_now_config_description")}</p>
-                {this.error && (
-                    <div className="notification notification--error">
-                        {this.error}
-                    </div>
-                )}
                 <div style={{marginTop: 20, marginBottom: 20}}>
                     <table  className="url-table">
                         <thead className="bg-gray-100">
@@ -92,36 +83,32 @@ class IndexNowConfig extends React.Component {
                         <table className="engine-table">
                             <thead className="engine-table-head">
                             <tr>
-                                <th>Batch</th>
                                 <th>Engine</th>
                                 <th>Status</th>
                                 <th>Response</th>
                             </tr>
                             </thead>
                             <tbody>
-                            {batchKeys.map((batchKey) => {
-                                const batchResponses = responses[batchKey] || {};
-                                return Object.keys(batchResponses).map((engine) => {
-                                    const res = batchResponses[engine];
-                                    const status = res?.status;
-                                    const success = status === 200 || status === 202;
-                                    return (
-                                        <tr key={`${batchKey}-${engine}`} className="engine-table-row">
-                                            <td className={`engine-table-cell ${success ? "success" : "error"}`}>
-                                                {batchKey}
-                                            </td>
-                                            <td className={`engine-table-cell ${success ? "success" : "error"}`}>
-                                                {engine}
-                                            </td>
-                                            <td className={`engine-table-cell ${success ? "success" : "error"}`}>
-                                                {status}
-                                            </td>
-                                            <td className={`engine-table-cell ${success ? "success" : "error"}`}>
-                                                {res?.body ? JSON.stringify(res.body) : ""}
-                                            </td>
-                                        </tr>
-                                    );
-                                });
+                            {engines.map((engine) => {
+                                const res = responsesMap[engine];
+                                const status = res?.status;
+                                const success = status === 200 || status === 202;
+                                return (
+                                    <tr key={engine} className="engine-table-row">
+                                        <td key={engine}
+                                            className={`engine-table-cell ${success ? "success" : "error"}`}>
+                                            {engine}
+                                        </td>
+                                        <td key={engine + status}
+                                            className={`engine-table-cell ${success ? "success" : "error"}`}>
+                                            {status}
+                                        </td>
+                                        <td key={engine + "body"}
+                                            className={`engine-table-cell ${success ? "success" : "error"}`}>
+                                            {res?.body ? JSON.stringify(res.body) : ""}
+                                        </td>
+                                    </tr>
+                                );
                             })}
                             </tbody>
                         </table>
